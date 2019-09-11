231bbb1e4df812b941b597073bec9afe0e241832
Damon Self <damongself@gmail.com>
2019-09-11 15:55:21 -0600

    adds battle methods. Again

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,4 +3,10 @@
 class Pokemon < ActiveRecord::Base
   has_many :pokeatts
   has_many :attacks, through: :pokeatts
+
+  def attacks
+    value = PokeAtt.all.select do |pokemonattacks|
+      pokemonattacks.pokemon.name == self.name
+    end
+  end
 end
