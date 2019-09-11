303eb166b5f627a68b30dbb5797e32553c80045c
Damon Self <damongself@gmail.com>
2019-09-11 10:05:32 -0600

    Updates environment file

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,14 @@
+# frozen_string_literal: true
+
 require 'bundler'
+require 'sinatra/activerecord'
+require 'require_all'
+require 'pry'
+require 'poke-api-v2'
 Bundler.require
 
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: 'db/pokemon.db'
+)
 require_all 'lib'
